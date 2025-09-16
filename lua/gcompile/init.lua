c5289c143501e0d51ae6d0c88ebe12f0f5694fed
peterjumper <lplssjuper@gmail.com>
2025-09-16 18:22:04 -0400

    update

--- lua/gcompile/init.lua
+++ lua/gcompile/init.lua
@@ -1,35 +1,39 @@
 local M = {}
-local config = require('gcompile.config')
+local config = require("gcompile.config")
 
 local userconf = {}
 
 function M.setup(user_config)
-    userconf = config.get_config(user_config)
+	userconf = config.get_config(user_config)
 
-    vim.api.nvim_create_user_command('GCompileAndRun', M.compile_and_run, {})
+	vim.api.nvim_create_user_command("GCompileAndRun", M.compile_and_run, {})
 
-    vim.api.nvim_create_user_command('GCompileRunAndExit', function()
-        M.compile_and_run(true)
-    end, {})
+	vim.api.nvim_create_user_command("GCompileRunAndExit", function()
+		M.compile_and_run(true)
+	end, {})
 end
 
 function M.compile_and_run(exit_on_end)
-    local file = vim.fn.expand("%:p")
-    if userconf.split == 'vertical' then
-        vim.cmd("vsplit | terminal")
-    else
-        vim.cmd("split | terminal")
-    end
+	local file = vim.fn.expand("%:p")
+	if userconf.split == "vertical" then
+		vim.cmd("vsplit | terminal")
+	else
+		vim.cmd("split | terminal")
+	end
 
-    local exit_part = ''
+	local exit_part = ""
 
-    if exit_on_end == true then
-        exit_part = 'exec '
-    end
+	if exit_on_end == true then
+		exit_part = "exec "
+	end
 
-    local command = ':call jobsend(b:terminal_job_id, "g++ -o a.out ' .. file .. ' && ' .. exit_part .. './a.out\\n")'
-    vim.cmd(command)
-    vim.cmd(":norm i")
+	local command = ':call jobsend(b:terminal_job_id, "g++ -o a.out '
+		.. vim.fn.shellescape(file)
+		.. " && "
+		.. exit_part
+		.. './a.out\\n")'
+	vim.cmd(command)
+	vim.cmd(":norm i")
 end
 
 return M
